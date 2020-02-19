4216f9b79c2be974e9220ade57a40353dca6173a
Kat <kmneil2@gmail.com>
2020-02-19 15:45:41 -0500

    Dice ramdom roll

--- CRC.md
+++ CRC.md
@@ -1,10 +1,8 @@
+Dice roll randomly outside of class
+
 Class: Game
 Responsibilties: Winner(score), score to total, turn, dice roll 1 switches players
 Collaborators: Die, Players turn, players score
-
-Class: Die
-Responsibilties: 6 sides, roll
-Collaborators: Game
 
 Class: Player
 Responsibilites: score, rolls to sum 
--- pig.py
+++ pig.py
@@ -1,19 +1,28 @@
+import random
+
+def roll():
+    score = random.choice([1,2,3,4,5,6])
+    print(score)
+    return score
+
+
 class Game:
   def __init__(self, score):
     self.score = score 
+    self.player = Player()
 
-    pass
-
-class Die:
-  def __init__(self):
-
-    pass
-
+    
+ 
 class Player:
-  def __init__(self, points):
-    self.points = points
+  def __init__(self, name):
+    self.name = name
+    self.points = 0
+    
+    
 
 class CompPlayer:
-  def __init__(self, points):
-    self.points = points
+  def __init__(self):
+    self.points = 0
+
   
+roll()
