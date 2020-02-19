abd2335b841ad47c5b84f0e4da4a2bad3386ccbf
Kat <kmneil2@gmail.com>
2020-02-19 13:50:59 -0500

    CRC.md file add

---
+++ pig.py
@@ -0,0 +1,19 @@
+class Game:
+  def __init__(self, score):
+    self.score = score 
+
+    pass
+
+class Die:
+  def __init__(self):
+
+    pass
+
+class Player:
+  def __init__(self, points):
+    self.points = points
+
+class CompPlayer:
+  def __init__(self, points):
+    self.points = points
+  
